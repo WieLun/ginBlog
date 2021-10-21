5ddc3851b063aa468fc3f8222f41eca80292dcf9
wielun <wielun666@gmail.com>
2021-10-21 10:26:59 +0800

    构建错误处理模块和路由接口

--- routes/route.go
+++ routes/route.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	v1 "ginblog/api/v1"
 	"ginblog/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -9,13 +10,16 @@ func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	router := r.Group("/api/v1")
+	rV1 := r.Group("/api/v1")
 	{
-		router.GET("/hello", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"msg": "ok",
-			})
-		})
+		// 用户模块路由接口
+		rV1.POST("/user/add", v1.AddUser)
+		rV1.GET("/users", v1.GetUsers)
+		rV1.PUT("/user/:id", v1.EditUser)
+		rV1.DELETE("/user/:id", v1.DeleteUser)
+		// 分类模块路由接口
+
+		// 文章模块路由接口
 	}
 	r.Run(utils.HttpPort)
 }
