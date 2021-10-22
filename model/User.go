3c880a0d7368c69bee3ba9ca9c36a7b6c715e953
wielun <wielun666@gmail.com>
2021-10-22 11:08:13 +0800

    删除和编辑用户信息

--- model/User.go
+++ model/User.go
@@ -27,7 +27,7 @@ func CheckUser(name string) int {
 
 // 新增用户
 func CreateUser(data *User) int {
-	data.Password = ScryptPw(data.Password)
+	//data.Password = ScryptPw(data.Password)
 	err := db.Create(&data).Error
 	if err != nil {
 		return errmsg.ERROR
@@ -45,16 +45,38 @@ func GetUsers(pageSize, pageNum int) []User {
 	return users
 }
 
-// 编辑用户
+// 编辑用户信息
+func EditUser(id int, data *User) int {
+	var maps = make(map[string]interface{})
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	err = db.Model(&User{}).Where("id = ?", id).Updates(maps).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
 
 // 删除用户
+func DeleteUser(id int) int {
+	err := db.Where("id = ?", id).Delete(&User{}).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// 密码加密
+func (u *User) BeforeSave() {
+	u.Password = ScryptPw(u.Password)
+}
 
 // 密码加密,生产最好用bcrypt
 func ScryptPw(password string) string {
 	const KeyLen = 10
 	salt := make([]byte, 8)
-	salt = []byte{1,23,83,12,13, 45, 35, 11}
-	HashPw,err := scrypt.Key([]byte(password), salt, 16384, 8, 1, KeyLen)
+	salt = []byte{1, 23, 83, 12, 13, 45, 35, 11}
+	HashPw, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, KeyLen)
 	if err != nil {
 		log.Fatal(err)
 	}
